cc02c4b49e9140248ab1e5e107977fb0fa2a0fff
Myeongjun Kim <audwnssp@naver.com>
2024-10-13 00:08:48 +0900

    자바 생성

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader read = new BufferedReader(new FileReader("data.txt"));
+        PrintWriter write = new PrintWriter("result.jsonl");
+
+        String question = "";
+        String answer = "";
+        String line = "";
+        int seperate = 0;
+
+        while(true) {
+            line = read.readLine();
+            if (line==null) break;
+
+            seperate = line.indexOf("###");
+            question = line.substring(0, seperate-1);
+            answer = line.substring(seperate+3);
+
+            System.out.println(line);
+
+            write.println(
+                    """
+                        {
+                            "messages": [
+                                {"role" : "system", "content":"Six Guy is a kind and friendly question assistant."},
+                                {"role" : "user", "content":
+                    """ + question + """ 
+                                },
+                                {"role" : "assistant", "content":
+                    """ + answer + """
+                            }
+                            ]
+                        }
+                    """ + "\n"
+            );
+        }
+
+        read.close();
+        write.close();
+    }
+}
